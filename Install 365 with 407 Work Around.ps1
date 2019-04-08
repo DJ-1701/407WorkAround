89fc9d32e39f9e1b0715bcd85e22c913972130e8
DJ-1701 <40062544+DJ-1701@users.noreply.github.com>
2019-04-08 14:42:30 +0100

    Add files via upload

---
+++ Install 365 with 407 Work Around.ps1
@@ -0,0 +1,18 @@
+﻿$OriginalData = Get-Content("C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Config\machine.config")
+$OriginalEnd = '</configuration>'
+$TempEnd = '<system.net><defaultProxy enabled="true" useDefaultCredentials="true"><proxy usesystemdefault="true" proxyaddress="PROXYDETAILSHERE:PORT" bypassonlocal="true"/></defaultProxy></system.net></configuration>'
+$TempData = $OriginalData -replace $OriginalEnd,$TempEnd
+
+If ((Get-DAConnectionStatus).Status -ne "ConnectedRemotely")
+{
+    If (!($OriginalData -like "*proxyaddress=*"))
+    {
+        If ($OriginalData -like "</configuration>")
+        {
+            Set-Content -Path "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Config\machine.config" -Value $TempData
+        }
+    }
+}
+.\OPPTransition.exe -Tenant TENANTUUID -Key KEYUUID -Domain DOMAIN
+$CorrectedData = $TempData -replace $TempEnd,$OriginalEnd
+Set-Content -Path "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Config\machine.config" -Value $CorrectedData
